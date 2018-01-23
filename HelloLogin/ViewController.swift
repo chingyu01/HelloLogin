ffda361e2ecbfeca072aff0881156b48a73c90de
freyahu131 <freyahu131@gmail.com>
2018-01-24 06:58:43 +0800

    login

--- HelloLogin/ViewController.swift
+++ HelloLogin/ViewController.swift
@@ -8,18 +8,54 @@
 
 import UIKit
 
+
+
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    @IBOutlet weak var usernameTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+
+    
+    @IBAction func loginButtonPressed(_ sender: Any) {
+     
+        if usernameTextField.text == "abc" && passwordTextField.text == "123"
+        {
+            performSegue(withIdentifier: "Right", sender: nil)
+//            if let Login = storyboard?.instantiateViewController(withIdentifier: "RightLoginViewController") as? RIghtLoginViewController{
+//
+//                show (Login, sender: nil)
+//            }
+//
+        }
+        
+        else {
+//            performSegue方便讓你判別login 從viewcontroller 拉線 show
+            performSegue(withIdentifier: "Wrong", sender: nil)
+//            if let Login = storyboard?.instantiateViewController(withIdentifier: "WrongLoginViewController") as? WrongLoginViewController{
+//
+//                show(Login, sender:nil)
+            
+                //checkVC.totalAmountLabel.text = "\(total)"
+            }
+    }
+        
+        
+        
+
+   override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
+    
+   
+     override  func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    }
 
-}
+
 
